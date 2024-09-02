250b535f6bb101cdd2d424f9defaaeaa2e60732f
Makarona10 <ahmedneyssi@gmail.com>
2024-09-03 00:28:36 +0300

    Task 3 done

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Auth class python file """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def __init__(self):
+        '''Auth class init function'''
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines whether a given path requires authentication or not"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''returns None - request will be the Flask request object'''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''request will be the Flask request object'''
+        return None
