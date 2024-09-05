b16e5cd8706f27721a511a1670e6117031cee0ff
Makarona10 <ahmedneyssi@gmail.com>
2024-09-06 02:57:50 +0300

    Task 9 done

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -18,7 +18,8 @@ class SessionExpAuth(SessionAuth):
             self.session_duration = 0
 
     def create_session(self, user_id=None):
-        session_id = super(user_id)
+        '''Creates a session id'''
+        session_id = super().create_session(user_id)
         if not session_id:
             return None
         SessionAuth.user_id_by_session_id[session_id] = {
@@ -28,6 +29,7 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
+        '''Returns user_id or None of no session_id'''
         s_dic = SessionAuth.user_id_by_session_id
         if not session_id or\
            session_id not in list(s_dic.keys()):
