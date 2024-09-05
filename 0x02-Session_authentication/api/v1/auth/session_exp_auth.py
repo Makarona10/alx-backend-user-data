7c8dfae48753a56e46bf7bf3d82536c0968be9a0
Makarona10 <ahmedneyssi@gmail.com>
2024-09-06 02:50:53 +0300

    Task 9 done

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" SessionExpAuth module """
+
+import os
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    '''SessionAuth class'''
+    def __init__(self):
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION'))
+            if not self.session_duration:
+                self.session_duration = 0
+        except TypeError:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        session_id = super(user_id)
+        if not session_id:
+            return None
+        SessionAuth.user_id_by_session_id[session_id] = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        s_dic = SessionAuth.user_id_by_session_id
+        if not session_id or\
+           session_id not in list(s_dic.keys()):
+            return None
+        if self.session_duration == 0:
+            return s_dic.get(session_id).get("user_id")
+        if not s_dic.get(session_id).get("created_at"):
+            return None
+        if (s_dic.get(session_id).get("created_at") +
+           timedelta(seconds=self.session_duration)) < datetime.now():
+            return None
+        return s_dic.get(session_id).get("user_id")
