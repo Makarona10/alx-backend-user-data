ca0adc507452afbf1b40a5d7b872dc308992b29b
Makarona10 <ahmedneyssi@gmail.com>
2024-09-05 19:18:56 +0300

    Task 8 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -36,9 +36,9 @@ class SessionAuth(Auth):
         if request is None:
             return False
         session_id = self.session_cookie(request)
-        if not session_id:
+        if session_id is None:
             return False
-        if not self.user_id_for_session_id(session_id):
+        if self.user_id_for_session_id(session_id) is None:
             return False
         del self.user_id_by_session_id[session_id]
         return True
