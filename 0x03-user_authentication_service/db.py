c5ff65ee8e933705396912a87185f231e96d4cd0
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 02:44:50 +0300

    Task 2 done

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        '''adds a new user to database'''
+        user = User(email=email, hashed_password=hashed_password)
+        self.__session.add(user)
+        self.__session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """find a user by passed arguments"""
+        for key in kwargs:
+            if not hasattr(User, key):
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
