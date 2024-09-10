cfac4f7ac12d18259ad13c5232f45ba441d75430
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 23:51:40 +0300

    Task 16 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,8 +75,8 @@ class Auth:
         """Resets password token"""
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = uuid4()
+            reset_token = str(uuid4())
             self._db.update_user(user.id, reset_token=reset_token)
-            return str(reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError
