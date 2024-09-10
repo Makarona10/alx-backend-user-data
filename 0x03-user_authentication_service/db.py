78276b140f4706eaccc5730995dc7f684bbac2cc
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 08:12:47 +0300

    Task 1 done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -56,13 +56,3 @@ class DB:
                 raise ValueError
         self._session.commit()
         return None
-
-my_db = DB()
-
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
-print(user_2.id)
-print(user_2.id)
