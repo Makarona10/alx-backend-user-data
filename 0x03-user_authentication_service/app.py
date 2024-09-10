17b0cdcb243ec111e378fc853f10d96c55f1f11d
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 23:36:37 +0300

    Task 14 done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,7 +45,7 @@ def login():
 @app.route("/sessions", methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     """Handles the logout process"""
-    session_id = request.cookies.session_id
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
