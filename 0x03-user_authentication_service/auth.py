dc5199e6ac2b762d44cd311150a761c4150ef367
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 23:35:11 +0300

    Task 14 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,4 +67,4 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """Gets rid of user session"""
-        self._db.update_user(user_id, session_id=None)
+        return self._db.update_user(user_id, session_id=None)
