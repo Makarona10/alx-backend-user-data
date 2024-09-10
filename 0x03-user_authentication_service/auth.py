1542ff91f84422b59790585a889bf9373066aeba
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 03:30:40 +0300

    Task 4 done

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Auth module'''
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Returns a salted hash of the input password"""
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
