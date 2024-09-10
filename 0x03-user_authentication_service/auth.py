6ef7844a5afd32217e06d9683c749739c1e6a282
Makarona10 <ahmedneyssi@gmail.com>
2024-09-10 23:44:28 +0300

    Task 15 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,11 +59,13 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """Retrieves a user according to session id"""
-        try:
-            user = self._db.find_user_by(session_id=session_id)
-            return user
-        except NoResultFound:
-            return None
+        if session_id is not None:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                pass
+        return None
 
     def destroy_session(self, user_id: int) -> None:
         """Gets rid of user session"""
